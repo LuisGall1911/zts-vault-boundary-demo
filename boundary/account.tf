c371faebdb056a2a8bea0d14430e2719782b6bdf
Luis Francisco Gall <105836215+LuisGall1911@users.noreply.github.com>
2023-03-23 11:48:37 -0300

    Update account.tf

--- boundary/account.tf
+++ boundary/account.tf
@@ -1,24 +1,6 @@
-resource "boundary_account_oidc" "luis" {
-  name           = "luis"
-  description    = "Luis"
-  auth_method_id = boundary_auth_method_oidc.auth0.id
-  issuer         = var.app_domain
-  subject        = var.auth0_user_id_luis
-  depends_on = [
-    boundary_auth_method_oidc.auth0
-  ]
-}
-
-resource "boundary_user" "luis" {
-  name        = "luis"
-  description = "Luis's user resource"
-  account_ids = [boundary_account_oidc.luis.id]
-  scope_id    = boundary_scope.org1.id
-}
-
-resource "boundary_managed_group" "boss" {
-  name           = "boss"
-  description    = "Boss OIDC managed group"
-  auth_method_id = boundary_auth_method_oidc.auth0.id
-  filter         = "\"luis\" in \"/userinfo/nickname\"
+resource "boundary_account_password" "luis" {
+  auth_method_id = boundary_auth_method.password.id
+  type           = "password"
+  login_name     = var.login_name
+  password       = var.password
 }
