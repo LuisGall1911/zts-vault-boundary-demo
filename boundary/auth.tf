a56d5f9c4816e79c6a3737baa92a469cc212b386
Luis Francisco Gall <105836215+LuisGall1911@users.noreply.github.com>
2023-03-27 15:58:34 -0300

    Update auth.tf

--- boundary/auth.tf
+++ boundary/auth.tf
@@ -1,4 +1,4 @@
-resource "boundary_auth_method_password" "userpass" {
+resource "boundary_auth_method_password" "password" {
    name               = "userpass"
    description        = "auth method with Username and Password"
    scope_id           = boundary_scope.org1.id
