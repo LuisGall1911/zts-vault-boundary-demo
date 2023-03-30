18a0e15029744303990bfd9ebe553344d54a58b1
Luis Francisco Gall <105836215+LuisGall1911@users.noreply.github.com>
2023-03-30 14:57:31 -0300

    Update account.tf

--- boundary/account.tf
+++ boundary/account.tf
@@ -1,5 +1,5 @@
 resource "boundary_account_password" "luis" {
-  auth_method_id = boundary_auth_method_password.password.id
+  auth_method_id = boundary_auth_method.password.id
   type           = "password"
   login_name     = var.login_name
   password       = var.password
