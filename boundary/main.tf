79cf50b51cba7ce2d31a9894db0509ea4de865c1
Luis Francisco Gall <105836215+LuisGall1911@users.noreply.github.com>
2023-03-30 14:27:14 -0300

    Update main.tf

--- boundary/main.tf
+++ boundary/main.tf
@@ -37,7 +37,7 @@ data "terraform_remote_state" "aws" {
 # HPC Boundary
 provider "boundary" {
   addr                            = data.terraform_remote_state.hcp.outputs.boundary_cluster_url
-  auth_method_id                  = var.auth_method_id            # changeme
+  #auth_method_id                  = var.auth_method_id            # changeme
   password_auth_method_login_name = var.boundary_cluster_username # changeme
   password_auth_method_password   = var.boundary_cluster_password # changeme
 }
